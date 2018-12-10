2237e5dfd24b351ec717f6ca15fbb7cd555a7c23
Hexman <834430486@qq.com>
2018-12-11 00:56:51 +0800

    Create check.py

---
+++ check.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import dns.resolver
+
+class subdomaintakeover:
+    def __init__(self,filename):
+        self.filename=filename
+
+    def check(self,domain):
+        try:
+            print("Check {domain}".format(domain=domain))
+            ans = dns.resolver.query(domain, 'a')
+        except:
+            print("[+]find vul --->{vuldomain}".format(vuldomain=domain))
+            with open("result.txt","a") as w:
+                w.write(domain+"\n")
+            return 0
+        for i in ans.response.answer:
+            for j in i.items:
+                if isinstance(j, dns.rdtypes.IN.A.A):
+                    return 0
+                if isinstance(j, dns.rdtypes.ANY.CNAME.CNAME):
+                    self.check(j.to_text())
+                return 0
+
+    def run(self):
+        with open(self.filename, 'r') as f:
+            for i in f.readlines():
+                self.check(i.strip())
+
+if __name__ == "__main__":
+    try:
+        filename=sys.argv[1]
+    except:
+        print('''
+        Usage: 
+            python main.py domainfile.txt
+        ''')
+        sys.exit()
+    if os.path.exists("result.txt"):
+        sys.stdout.write("result.txt exists, Do u want to replace it? [Y/n]:")
+        scantype = sys.stdin.readline()
+        if scantype=="n":
+            print("Bye")
+        else:
+            os.remove("result.txt")
+            subdomaintakeover(filename=filename).run()
+    else:
+        subdomaintakeover(filename=filename).run()
